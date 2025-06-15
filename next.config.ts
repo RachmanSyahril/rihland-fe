e362f8c30e28d216afb2d672803325906bef462d
Syahril Rachman <rachmansyahril@gmail.com>
2025-06-15 20:18:23 +0700

    update basepath

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "https://rachmansyahril.github.io/rihland-fe",
+  basePath: "/rihland-fe",
 
   /**
    * Disable server-based image optimization. Next.js does not support
